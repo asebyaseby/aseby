81ab020eba625685ff26c40876caec3a01b60f15
unknown <1958423948@qq.com>
2023-08-30 08:58:15 +0800

    aa

---
+++ src/main/java/com/example/demo/controller/DataViewController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+@RestController
+public class DataViewController {
+ @RequestMapping("/taskdemo")
+ public ModelAndView itemsList() {
+ ModelAndView mav = new ModelAndView("/views/HelloWorld.html");
+ return mav;
+ }
+ @RequestMapping("/bar")
+ public ModelAndView itemList() {
+        ModelAndView mav = new ModelAndView("/views/BarChart.html");
+        return mav;
+ }
+ @RequestMapping("/radar")
+ public ModelAndView radarList() {
+        ModelAndView mav = new ModelAndView("/views/RadarChart.html");
+        return mav;
+    }
+    @RequestMapping("/test")
+    public ModelAndView indexList() {
+        ModelAndView mavi = new ModelAndView("/test.html");
+        return mavi;
+    }
+}
---
+++ src/test/java/com/example/demo/trytry.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import net.minidev.json.JSONUtil;
+
+public class trytry{
+
+    public static void main(String[] args) {
+for (int i = 0;i<34;i++){
+    System.out.println(Math.random() * 100 + 10);
+}
+
+
+    }
+
+}
+
